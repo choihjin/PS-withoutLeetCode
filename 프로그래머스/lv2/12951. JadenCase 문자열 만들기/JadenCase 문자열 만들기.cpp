eda580c2d800629a891b4baff64bc904e536b12f
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-01-10 23:19:26 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    for(int i=0; i<s.size(); i++) {
+        if(i == 0 || s[i-1] == ' ') answer += toupper(s[i]);
+        else answer += tolower(s[i]);
+    }
+    
+    return answer;
+}
