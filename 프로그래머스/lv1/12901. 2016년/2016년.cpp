ebac1cf958fc97a1c233ce648694c8cbe458eb7a
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-01-16 14:47:20 +0900

    Update 2016년.cpp

--- 프로그래머스/lv1/12901. 2016년/2016년.cpp
+++ 프로그래머스/lv1/12901. 2016년/2016년.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 string solution(int a, int b) {
-    int month[] = {31,29,31,30,31,30,31,31,30,31,30,31};
-    string day[] = {"THU","FRI","SAT","SUN","MON","TUE","WED"};
+    int month[] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    string day[] = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
     
-    int sum = b;
+    int sum = b-1;
     
     for(int i=0; i<a-1; i++){
         sum += month[i];
