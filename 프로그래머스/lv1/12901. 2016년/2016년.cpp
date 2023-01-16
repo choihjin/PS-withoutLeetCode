7a4551e2dfd8ae2773a86353b4dca668e2620b8f
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-01-16 14:44:20 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int a, int b) {
+    int month[] = {31,29,31,30,31,30,31,31,30,31,30,31};
+    string day[] = {"THU","FRI","SAT","SUN","MON","TUE","WED"};
+    
+    int sum = b;
+    
+    for(int i=0; i<a-1; i++){
+        sum += month[i];
+    }
+
+    return day[sum%7];
+}
