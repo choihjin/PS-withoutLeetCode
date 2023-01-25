6e614d0b5872d3d5e53831f6e838b8280a8b99b7
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-01-25 11:17:36 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+  
+  for(int i=0; i<n; i++) {
+    for(int j=0; j<i; j++) {
+      cout << " ";
+    }
+    for(int j=n-i; j>0; j--) {
+        cout << "*";
+    }
+    for(int j=n-i-1; j>0; j--) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+}
