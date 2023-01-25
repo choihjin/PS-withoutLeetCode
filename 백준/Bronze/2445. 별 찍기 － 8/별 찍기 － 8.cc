1712e1c9b4bd5fb1e4e43291f23b34bff93a5757
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-01-25 11:42:06 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+  
+  for(int i=0; i<n; i++) {
+    for(int j=0; j<=i; j++) {
+      cout << "*";
+    }
+    for(int j=n-i-1; j>0; j--) {
+      cout << " ";
+    }
+    for(int j=n-1; j>i; j--) {
+      cout << " ";
+    }
+    for(int j=0; j<=i; j++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+
+  for(int i=0; i<n; i++) {
+    for(int j=n-i-1; j>0; j--) {
+      cout << "*";
+    }
+    for(int j=0; j<=i; j++) {
+      cout << " ";
+    }
+    for(int j=0; j<=i; j++) {
+      cout << " ";
+    }
+    for(int j=n-i-1; j>0; j--) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+}
