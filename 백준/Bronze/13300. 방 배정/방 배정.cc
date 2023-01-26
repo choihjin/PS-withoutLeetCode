0889b854072ee5e18833a3b7c2bbb0260ddea82c
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-01-26 15:31:26 +0900

    Update 방 배정.cc

--- 백준/Bronze/13300. 방 배정/방 배정.cc
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -6,10 +6,12 @@ int main() {
   cin.tie(0);
 
   int n, k;
+  //성별로 나누고 각 학년마다 인원수를 저장하는 배열
   int male[6]= {0, };
   int female[6] = {0, };
   
   cin >> n >> k;
+  //학생 수 저장
   while(n--) {
     int gender;
     int grade;
@@ -17,7 +19,8 @@ int main() {
     if(gender) male[grade-1]++;
     else female[grade-1]++;
   }
-
+  
+  //필요한 방의 개수 카운트
   int count = 0;
   for(int i=0; i<6; i++) {
     if(male[i]%k == 0) count += male[i]/k;
