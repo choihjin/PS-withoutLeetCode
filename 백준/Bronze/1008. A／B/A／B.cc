0bc0364abb84c56fd7e384dbde2be0b6f72b9282
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-02-08 22:01:57 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    double a, b;
+    cin >> a >> b;
+
+    cout.precision(15);
+    cout << a / b << "\n";
+}
