ff4f1ae937f384caac7ac9039fd6295cb607b158
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-02-08 23:12:40 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+
+void func(int k) {
+    if(k == m) {
+        for(int i=0; i<m; i++) cout << arr[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    for(int i=1; i<=n; i++) {
+        if(k != 0 && arr[k-1] > i) continue; // 비내림차순 조건
+        arr[k] = i;
+        func(k+1);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    func(0);
+}
