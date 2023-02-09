6847eb64daf66ae0c71fad8672ef468d1de79fdb
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-02-09 14:06:41 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+int ans[10];
+bool isused[10];
+int pre; // 백트래킹 했을 때 그 전의 값을 저장해둠
+
+void func(int k) {
+  if(k == m) {
+    for(int i=0; i<m; i++) cout << ans[i] << " ";
+    cout << "\n";
+    return;
+  }
+  
+  pre = 0x7f7f7f7f; // k가 바뀌었으면 pre값 초기화
+  for(int i=0; i<n; i++) {
+    if(!isused[i] && pre != arr[i]) {
+      if(k != 0 && ans[k-1] > arr[i]) continue;
+      isused[i] = 1;
+      ans[k] = arr[i];
+      func(k+1);
+      isused[i] = 0;
+      pre = arr[i];
+    }
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n >> m;
+  for(int i=0; i<n; i++) cin >> arr[i];
+  sort(arr, arr+n);
+  func(0);
+}
