12bf25a229f3f342eac52f6651d2b4a8c3dcf36c
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-02-09 16:13:41 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  while(1) {
+      cin >> a >> b;
+      if(cin.eof() == true) break;
+      cout << a+b << "\n";
+  }
+}
