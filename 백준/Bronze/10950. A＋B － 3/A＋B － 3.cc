3e69038dbf20c701bdcf51ce2e68832418a45d56
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-02-09 15:49:37 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, a, b;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    while(n--) {
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+}
