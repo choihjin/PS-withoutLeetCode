186ac2f3f138d52ad3b388df8980c051e1d02bb6
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-02-09 15:56:00 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, a, b;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        cin >> a >> b;
+        cout << "Case #" << i+1 << ": " << a+b << "\n";
+    }
+}
