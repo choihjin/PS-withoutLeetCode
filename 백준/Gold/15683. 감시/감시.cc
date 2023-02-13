eea199c579a982f0c50aefe89ffecc5f7ec52f84
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-02-13 15:21:20 +0900

    [Gold IV] Title: 감시, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/15683. 감시/감시.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1}; // 남쪽, 동쪽, 북쪽, 서쪽 순서
+int n, m;
+int board1[10][10]; // 최초에 입력받은 board를 저장할 변수
+int board2[10][10]; // 사각 지대의 개수를 세기 위해 사용할 변수
+vector<pair<int,int> > cctv; // cctv의 좌표를 저장할 변수
+
+void upd(int x, int y, int dir) {
+  dir %= 4;
+  while(1) {
+    x += dx[dir];
+    y += dy[dir];
+    if(x < 0 || x >= n || y < 0 || y >= m || board2[x][y] == 6) break;
+    if(board2[x][y] != 0) continue;
+    board2[x][y] = 7;
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> m;
+  int mn = 0;
+  for(int i=0; i<n; i++) {
+    for(int j=0; j<m; j++) {
+      cin >> board1[i][j];
+      if(board1[i][j] > 0 && board1[i][j] < 6) cctv.push_back({i, j});
+      if(board1[i][j] == 0) mn++;
+    }
+  }
+
+  for(int tmp=0; tmp < (1<<(2*cctv.size())); tmp++) {
+    for(int i=0; i<n; i++)
+      for(int j=0; j<m; j++)
+        board2[i][j] = board1[i][j];
+    
+    int brute = tmp;
+    for(int i=0; i<cctv.size(); i++) {
+      int dir = brute%4;
+      brute /= 4;
+      int x, y;
+      tie(x, y) = cctv[i];
+      if(board1[x][y] == 1) {
+        upd(x, y, dir);
+      }
+      else if(board1[x][y] == 2) {
+        upd(x, y, dir);
+        upd(x, y, dir+2);
+      }
+      else if(board1[x][y] == 3) {
+        upd(x, y, dir);
+        upd(x, y, dir+1);
+      }
+      else if(board1[x][y] == 4) {
+        upd(x, y, dir);
+        upd(x, y, dir+1);
+        upd(x, y, dir+2);
+      }
+      else if(board1[x][y] == 5) {
+        upd(x, y, dir);
+        upd(x, y, dir+1);
+        upd(x, y, dir+2);
+        upd(x, y, dir+3);
+      }
+    }
+
+    int val = 0;
+    for(int i=0; i<n; i++)
+      for(int j=0; j<m; j++)
+        if(board2[i][j] == 0) val++;
+    mn = min(val, mn);
+  }
+  cout << mn << "\n";
+}
