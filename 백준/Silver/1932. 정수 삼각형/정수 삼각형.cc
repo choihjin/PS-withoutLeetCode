ec4b22d491273bf5041f579c90452918509aa5df
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-02-21 20:17:40 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 4012 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int d[505][505];
+int a[505][505];
+int n;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n;
+    for(int i = 1; i <= n; ++i)
+        for(int j = 1; j <= i; ++j)
+            cin >> a[i][j];
+
+    d[1][1] = a[1][1];
+    for(int i = 2; i <= n; ++i)
+        for(int j = 1; j <= i; ++j)
+            d[i][j] = max(d[i-1][j-1], d[i-1][j]) + a[i][j];
+
+    cout << *max_element(d[n] + 1, d[n] + n + 1);
+}
