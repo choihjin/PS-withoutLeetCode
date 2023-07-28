86ebceb479f2ddff26adee8b807f8c2b190603b9
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-07-28 17:02:50 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, ans;
+int P[1002];
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N;
+  for (int i = 0; i < N; i++) cin >> P[i];
+  sort(P, P + N);
+  for (int i = 0; i < N; i++) ans += P[i] * (N - i);
+  cout << ans;
+}
