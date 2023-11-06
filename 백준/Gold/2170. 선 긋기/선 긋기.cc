c5843d1164d5461fe1c998fd469416d506d57449
s21900772 <103234587+choihjin@users.noreply.github.com>
2023-11-07 00:37:48 +0900

    [Gold V] Title: 선 긋기, Time: 364 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.cc
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -1,29 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define endl "\n"
 #define X first
 #define Y second
 
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  vector<pair<int,int>> events;  
-  int n;
-  cin >> n;
-  while(n--){
-    int l, r;
-    cin >> l >> r;
-    events.push_back({l, 1});
-    events.push_back({r, -1});
-  }
-  sort(events.begin(), events.end());
-  int cnt = 0; // 현재 선의 개수
-  int tot = 0; // 전체 길이(= 답)
-  int loc = -1e9; // 현재 위치
-  for(auto event : events){
-    if(cnt > 0) tot += (event.X - loc);
-    loc = event.X;
-    cnt += event.Y;
-  }
-  cout << tot;
+int n;
+vector<pair<int, int>> lines;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        int x, y;
+        cin >> x >> y;
+        lines.push_back({x, y});
+    }
+    sort(lines.begin(), lines.end());
+    
+    int ans = 0;
+    pair<int, int> prev = lines[0];
+    for(int i=1; i<n; i++) {
+        pair<int, int> cur = lines[i];
+        if(prev.Y >= cur.X && prev.Y <= cur.Y) prev.Y = cur.Y;
+        else if(cur.X > prev.Y) {
+            ans += prev.Y - prev.X;
+            prev.X = cur.X;
+            prev.Y = cur.Y;
+        }
+    }
+
+    cout << ans + prev.Y - prev.X << endl;
+
+    return 0;
 }
