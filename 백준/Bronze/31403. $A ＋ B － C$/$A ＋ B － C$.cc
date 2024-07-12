feefbd8a57e43ae0709e4eedbcdc1e93782ccd68
s21900772 <103234587+choihjin@users.noreply.github.com>
2024-07-12 10:20:56 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    cout << a+b-c << endl;
+    string str = to_string(a) + to_string(b);
+    cout << stoi(str) - c << endl;
+    
+    return 0;
+}
