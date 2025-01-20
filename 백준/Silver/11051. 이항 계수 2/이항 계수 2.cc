9e51d8b5d006eef1519167286e59b1ecba732c58
s21900772 <103234587+choihjin@users.noreply.github.com>
2025-01-20 17:07:00 +0900

    [Silver II] Title: 이항 계수 2, Time: 4 ms, Memory: 5944 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
@@ -2,23 +2,18 @@
 using namespace std;
 #define endl "\n"
 
-int dp[1002][1002];
+int comb[1002][1002];
+int mod = 10007;
 
 int main() {
-    ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-
     int n, k;
     cin >> n >> k;
-
+    
     for(int i=1; i<=n; i++) {
-        for(int j=0; j<=k; j++) {
-            if (j == 0 || i == j) dp[i][j] = 1;
-			else dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]) % 10007;
+        comb[i][0] = comb[i][i] = 1;
+        for(int j=1; j<i; j++) {
+            comb[i][j] = (comb[i-1][j] + comb[i-1][j-1]) % mod;
         }
     }
-
-    cout << dp[n][k] << endl;
-
-    return 0;
+    cout << comb[n][k] << endl;
 }
