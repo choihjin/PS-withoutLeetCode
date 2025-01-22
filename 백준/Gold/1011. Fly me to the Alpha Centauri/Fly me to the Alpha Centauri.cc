82158fe160c912a899ce0e966f799c97505cfbfd
s21900772 <103234587+choihjin@users.noreply.github.com>
2025-01-22 14:57:31 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
@@ -14,31 +14,26 @@ int main() {
         int pos = 0;
         int sum = 0;
         while(x < y) {
-            // cout << "x : " << x << " ans : " << ans << " pos : " << pos << " sum : " << sum << endl;
             // pos + 1 case
             if(x + sum + pos+1 <= y) {
                 pos++;
-                // cout << "pos+1 case : +" << pos << endl;
                 ans++;
                 x += pos;
                 if(pos >= 1) sum += pos;
             }
             // pos case
             else if(x + sum <= y) {
-                // cout << "pos case : +" << pos << endl;
                 ans++;
                 x += pos;
             }
             // pos - 1 case
             else {
-                // cout << "pos-1 case : +" << pos-1 << endl;
                 ans++;
                 if(sum > 1) sum -= pos;
                 if(pos != 1) pos--;
                 x += pos;
             }
         }
-        // cout << "x : " << x << " ans : " << ans << " pos : " << pos << " sum : " << sum << endl;
         cout << ans << endl;
     }
 }
