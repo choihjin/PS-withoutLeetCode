fa437be3a57eb2b118b3be6712e51bab8e28ce6c
choihjin <103234587+choihjin@users.noreply.github.com>
2025-03-03 01:27:42 +0900

    [Gold IV] Title: LCS 3, Time: 152 ms, Memory: 119696 KB -BaekjoonHub

---
+++ 백준/Gold/1958. LCS 3/LCS 3.py
@@ -0,0 +1,14 @@
+s1 = input()
+s2 = input()
+s3 = input()
+dp = [[[0] * (len(s3) + 1) for _ in range(len(s2) + 1)] for __ in range(len(s1) + 1)]
+
+for i in range(1, len(s1) + 1):
+    for j in range(1, len(s2) + 1):
+        for k in range(1, len(s3) + 1):
+            if s1[i-1] == s2[j-1] and s2[j-1] == s3[k-1]:
+                dp[i][j][k] = dp[i-1][j-1][k-1] + 1
+            else:
+                dp[i][j][k] = max(dp[i-1][j][k], dp[i][j-1][k], dp[i][j][k-1])
+
+print(dp[len(s1)][len(s2)][len(s3)])
