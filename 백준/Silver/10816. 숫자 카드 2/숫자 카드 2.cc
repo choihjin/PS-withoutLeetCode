7ca4dcac3a7ec631649923bfc684ea2e35ebc2da
choihjin <jjin6573@naver.com>
2025-03-10 16:05:15 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 296 ms, Memory: 3972 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int n, m;
+int arr[500002];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for(int i=0; i<n; i++) cin >> arr[i];
+    sort(arr, arr+n);
+
+    cin >> m;
+    for(int i=0; i<m; i++) {
+        int tmp;
+        cin >> tmp;
+        cout << upper_bound(arr, arr+n, tmp) - lower_bound(arr, arr+n, tmp) << " ";
+    }
+
+    return 0;
+}
