4b12ac96a0238eadca7079eb990f88f4fa44f972
choihjin <jjin6573@naver.com>
2025-03-10 15:40:07 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+typedef long long ll;
+int k, n;
+int arr[10005];
+
+bool solve(ll x) {
+    ll cur = 0;
+    for(int i=0; i<k; i++) cur += arr[i] / x;
+    return cur >= n;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> k >> n;
+    for(int i=0; i<k; i++) cin >> arr[i];
+
+    ll st = 1;
+    ll en = 0x7fffffff;
+    while(st < en) {
+        ll mid = (st + en + 1) / 2;
+        if(solve(mid)) st = mid;
+        else en = mid - 1;
+    }
+    cout << st << endl;
+
+    return 0;
+}
